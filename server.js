2c80485e93cc5e0412b53e8461307b9b4f067423
bytrustu <bytrustu@gmail.com>
2020-10-13 04:44:12 +0900

    setting mongo db

--- server.js
+++ server.js
@@ -7,6 +7,20 @@ const app = express();
 app.use(express.json());
 
 const productRoutes = require('./routes');
+const mongoose = require('mongoose');
+
+const password = 'dydwns89';
+const dbName = 'tdd';
+const uri = `mongodb+srv://bytrustu:${password}@cluster0.erj1u.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('success');
+}).catch(e => {
+    console.log(e);
+})
+
 
 app.use('/api/products', productRoutes);
 
